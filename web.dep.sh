aeb61e30d6f62d5425bf4e12d885b6447c2fb903
ops2066 <nosh19950626@gmail.com>
2025-04-25 05:24:52 -0400

    Create web.dep.sh

---
+++ web.dep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo tar -zxvf /opt/dow/build.tar.gz -C /opt/www/web
+cd /opt/www/web
+sudo chown -R www-data:www-data /opt/www
+sudo chmod -R 755 /opt/www
+pm2 reload ecosystem.config.js
+
+cd /opt/dow/
+timestamp=$(date +"%Y-%m-%d-%H-%M")
+src_file="build.tar.gz"
+new_file="${timestamp}-${src_file}"
+mv "$src_file" "$new_file"
