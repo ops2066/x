295a154f1a30e14f994ed960369c5ec6ef200f4e
ops2066 <nosh19950626@gmail.com>
2025-04-25 04:18:07 -0400

    Update api.dep.sh

--- api.dep.sh
+++ api.dep.sh
@@ -11,3 +11,5 @@ mv "$src_file" "$new_file"
 echo "new file name：$new_file"
 sudo systemctl restart tv-api.service
 sudo systemctl status tv-api.service
+sudo systemctl restart tv-admin.service
+sudo systemctl status tv-admin.service
