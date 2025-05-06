b2d3251cb6373447e1da5a9a025d8589592e1975
ops2066 <nosh19950626@gmail.com>
2025-05-06 04:04:25 -0400

    Create bot-api-dep.sh

---
+++ bot-api-dep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /opt/dow/
+sudo tar -zxvf /opt/dow/app.tar.gz -C /opt/game/api
+sudo chown -R www-data:www-data /opt/game
+sudo chmod -R 755 /opt/game
+sudo chmod +x /opt/game/api/bin/app
+timestamp=$(date +"%Y-%m-%d-%H-%M")
+src_file="app.tar.gz"
+new_file="${timestamp}-${src_file}"
+mv "$src_file" "$new_file"
+echo "new file name:$new_file"
+sudo systemctl restart bot-api.service
+sudo systemctl status bot-api.service
+sudo systemctl restart bot-admin.service
+sudo systemctl status bot-admin.service
